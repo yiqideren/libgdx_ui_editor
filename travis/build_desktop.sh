8e54cb3a911442207f58b6b0c1774af9b69a9981
whitecostume <1003781706@qq.com>
2017-05-01 16:17:11 +0800

    5/1 2

--- travis/build_desktop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [[ $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_REPO_SLUG == 'whitecostume/libgdx_ui_editor' && $TRAVIS_BRANCH == 'master' ]];
-then
-    gradlew desktop:dist
-fi
